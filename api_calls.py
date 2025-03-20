89034d4b75cfdb95fa384ff4d8fb026962108962
Obinna Ofomah <jano.obinna@gmail.com>
2025-03-20 04:06:41 +0000

    completed the api calls project

---
+++ api_calls.py
@@ -0,0 +1,61 @@
+######################################################################
+# Task 1 - Extract all the senior and managerial roles 
+
+import requests 
+roles_url = 'https://jobicy.com/api/v2/remote-jobs?count=20&geo=usa&industry=marketing&tag=seo'
+response = requests.get(roles_url)
+roles_data = response.json()['jobs']
+
+managerial_roles = []
+senior_roles = []
+
+for role in roles_data:
+    # since both jobLevels are Senior level roles, we start the condition from jobLevel == senior 
+    if role['jobLevel'] == 'Senior':
+        
+        # A role is a managerial level role if the word 'Manager' appears in the jobTitle
+        if 'Manager' in role['jobTitle']:
+            managerial_roles.append(role)
+        else:
+            senior_roles.append(role)
+
+###############################################################################
+# Task 2 - Extract all the competitions from the api
+
+football_api = 'http://api.football-data.org/v4/competitions/'
+football_response = requests.get(football_api)
+competitions_data = football_response.json()['competitions']
+
+competition_list = []
+
+for competition in competitions_data:
+    competition_list.append(competition['name'])
+
+
+
+###############################################################################
+# Task 3 -  Extract all the male and female profiles into different lists
+
+profiles_url = 'https://randomuser.me/api/?results=500'
+profiles_data = requests.get(profiles_url).json()['results']
+
+males_profile = []
+females_profile = []
+dates_of_birth = []
+full_names = []
+
+for profile in profiles_data:
+    # appending the dates_of_birth for each profile
+    dates_of_birth.append(profile['dob']['date'])
+
+    # appending the concatenated full name into a list
+    first_name = profile['name']['first']
+    last_name = profile['name']['last']
+    full_name = first_name + ' ' + last_name
+    full_names.append(full_name)
+
+    # Spliting the profiles into males and females
+    if profile['gender'] == 'female':
+        females_profile.append(profile)
+    elif profile['gender'] == 'male':
+        males_profile.append(profile)
